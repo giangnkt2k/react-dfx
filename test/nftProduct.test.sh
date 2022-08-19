f3a89cc4679ce89791c7d61dfbfb36151b090b6b
duongptryu <duongptryu@gmail.com>
2022-08-19 08:57:22 +0700

    feat: fix bug backend and write test

--- test/nftProduct.test.sh
+++ test/nftProduct.test.sh
@@ -83,7 +83,7 @@ let resp = call marketplaceCanister.AddOrder(
         stepBid=1000;
         startPrice=20000;
         tokenId=opt 1; 
-        auctionTime=86400; 
+        auctionTime=86400000000000; 
         tokenPayment=dip20Canister; 
         typeAuction=variant {AuctionNFT}; 
         metadataAuction=null
@@ -102,17 +102,20 @@ let resp = call marketplaceCanister.AddOrder(
         stepBid=1000;
         startPrice=20000;
         tokenId=opt 2; 
-        auctionTime=86400; 
+        auctionTime=17000000000; 
         tokenPayment=dip20Canister; 
         typeAuction=variant {AuctionNFT}; 
         metadataAuction=null
     }
 );
 assert resp == variant { Ok = 3 : nat };
+let resp = call dip721Canister.ownerOf(2);
+assert resp == opt marketplaceCanister;
 
 "Bid product";
 let amountBid1 = 25000;
 let amountBid2 = 30000;
+let amountBid3 = 35000;
 let amountLarge = 2000000;
 let amountSmall = 500;
 " - Should revert if auction not exist";
@@ -147,4 +150,117 @@ let resp = call dip20Canister.approve(marketplaceCanister, amountBid2);
 assert resp == variant { Ok = 6 : nat };
 let resp = call marketplaceCanister.BidAuction(record {auctionId=2; amount=amountBid2});
 assert resp == variant { Ok = 2 : nat };
+" - Should work correctly 3";
+let resp = call dip20Canister.approve(marketplaceCanister, amountBid1);
+assert resp == variant { Ok = 8 : nat };
+let resp = call marketplaceCanister.BidAuction(record {auctionId=3; amount=amountBid1});
+assert resp == variant { Ok = 1 : nat };
+
+
+
+"Cancel order";
+identity account2 "../config/account2.pem";
+" - Should revert if order not exist";
+let resp = call marketplaceCanister.CancelOrder(4);
+assert resp == variant { Err = variant { AuctionNotExist } };
+" - Should revert if not owner of order";
+let resp = call marketplaceCanister.CancelOrder(2);
+assert resp == variant { Err = variant { NotOwnerOfOrder } };
+" - Should revert if order already has bid";
+identity account1 "../config/account1.pem";
+let resp = call marketplaceCanister.CancelOrder(2);
+assert resp == variant { Err = variant { CannotCancelOrder } };
+" - Should revert if order already finish";
+identity account2 "../config/account2.pem";
+let resp = call marketplaceCanister.CancelOrder(1);
+assert resp == variant { Err = variant { OrderAlreadyFinish } };
+" - Should work correctly";
+
+identity account1 "../config/account1.pem";
+let resp = call dip721Canister.mint(record {url="http://groupbar.me/4"; name="Duong4"; description="Day la anh cua Duong4"});
+assert resp == variant { Ok = 4 : nat };
+let resp = call marketplaceCanister.AddOrder(
+    record {
+        stepBid=1000;
+        startPrice=20000;
+        tokenId=opt 4; 
+        auctionTime=50000000000; 
+        tokenPayment=dip20Canister; 
+        typeAuction=variant {AuctionNFT}; 
+        metadataAuction=null
+    }
+);
+assert resp == variant { Ok = 4 : nat };
+
+let resp = call marketplaceCanister.CancelOrder(4);
+assert resp == variant { Ok = true : bool };
+let resp = call dip721Canister.ownerOf(4);
+assert resp == opt account1;
+
+"Refund token";
+identity account3 "../config/account3.pem";
+let resp = call dip20Canister.approve(marketplaceCanister, amountBid3);
+assert resp == variant { Ok = 10 : nat };
+let resp = call marketplaceCanister.BidAuction(record {auctionId=2; amount=amountBid3});
+assert resp == variant { Ok = 3 : nat };
+" - should revert if auction not exist";
+let resp = call marketplaceCanister.RefundToken(67, 2);
+assert resp == variant { Err = variant { AuctionNotExist } };
+" - Should revert if bid of auction not exist";
+let resp = call marketplaceCanister.RefundToken(2, 67);
+assert resp == variant { Err = variant { BidNotExist } };
+" - Should revert if not owner of offer";
+let resp = call marketplaceCanister.RefundToken(2, 2);
+assert resp == variant { Err = variant { NotOwnerOfBid } };
+" - Should revert if the bid is the highest bid";
+let resp = call marketplaceCanister.RefundToken(2, 3);
+assert resp == variant { Err = variant { ErrCannotRefundHighestBid } };
+" - Should work correctly";
+identity account2 "../config/account2.pem";
+let beforeBalance = call dip20Canister.balanceOf(marketplaceCanister);
+beforeBalance;
+let resp = call marketplaceCanister.RefundToken(2, 2);
+assert resp == variant { Ok = true : bool };
+let resp = call dip20Canister.balanceOf(account2);
+resp;
+" - Should revert if already refund bid";
+let resp = call marketplaceCanister.RefundToken(2, 2);
+assert resp == variant { Err = variant { BidAlreadyRefund } };
+
+"Claim token";
+identity account3 "../config/account3.pem";
+"- Should revert if auction not exist";
+let resp = call marketplaceCanister.ClaimToken(67);
+assert resp == variant { Err = variant { AuctionNotExist } };
+"- Should revert if time is not finish";
+identity account2 "../config/account2.pem";
+let resp = call marketplaceCanister.ClaimToken(2);
+assert resp == variant { Err = variant { NotSeller } };
+"- Should revert if not owner of auction";
+identity account3 "../config/account3.pem";
+let resp = call marketplaceCanister.ClaimToken(2);
+assert resp == variant { Err = variant { NotSeller } };
+"- Should work correctly";
+identity account2 "../config/account2.pem";
+let resp = call marketplaceCanister.ClaimToken(3);
+assert resp == variant { Ok = true : bool };
+let resp = call dip20Canister.balanceOf(account2);
+resp;
+
+"Claim NFT";
+identity account2 "../config/account2.pem";
+" - Should revert if auction not exist";
+let resp = call marketplaceCanister.ClaimToken(67);
+assert resp == variant { Err = variant { AuctionNotExist } };
+" - Should revert if time is not finish";
+let resp = call marketplaceCanister.ClaimToken(2);
+assert resp == variant { Err = variant { TimeAuctionNotEnd } };
+" - Should revert if not winner";
+identity account1 "../config/account1.pem";
+let resp = call marketplaceCanister.ClaimToken(3);
+assert resp == variant { Err = variant { NotSeller } };
+" - Should work correctly";
+identity account3 "../config/account3.pem";
+let resp = call marketplaceCanister.ClaimToken(3);
+assert resp == variant { Ok = true : bool };
 

eda479432572a42119af0e63a1c996c7c15d6aad
AnhNQ2710 <crow18120@gmail.com>
2022-08-20 00:22:27 +0700

    [AnhNQ2710] Update form create seller and add formik to handler form.

--- frontend/components/MKAvatar/MKAvatarRoot.jsx
+++ frontend/components/MKAvatar/MKAvatarRoot.jsx
@@ -12,27 +12,27 @@ Coded by www.creative-tim.com
 
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
-import React from 'react';
+import React from "react"
 // @mui material components
-import Avatar from "@mui/material/Avatar";
-import { styled } from "@mui/material/styles";
+import Avatar from "@mui/material/Avatar"
+import { styled } from "@mui/material/styles"
 
 export default styled(Avatar)(({ theme, ownerState }) => {
-  const { palette, functions, typography, boxShadows } = theme;
-  const { shadow, bgColor, size } = ownerState;
+  const { palette, functions, typography, boxShadows } = theme
+  const { shadow, bgColor, size } = ownerState
 
-  const { gradients, transparent, white } = palette;
-  const { pxToRem, linearGradient } = functions;
-  const { size: fontSize, fontWeightRegular } = typography;
+  const { gradients, transparent, white } = palette
+  const { pxToRem, linearGradient } = functions
+  const { size: fontSize, fontWeightRegular } = typography
 
   // backgroundImage value
   const backgroundValue =
     bgColor === "transparent"
       ? transparent.main
-      : linearGradient(gradients[bgColor].main, gradients[bgColor].state);
+      : linearGradient(gradients[bgColor].main, gradients[bgColor].state)
 
   // size value
-  let sizeValue;
+  let sizeValue
 
   switch (size) {
     case "xs":
@@ -40,42 +40,49 @@ export default styled(Avatar)(({ theme, ownerState }) => {
         width: pxToRem(24),
         height: pxToRem(24),
         fontSize: fontSize.xs,
-      };
-      break;
+      }
+      break
     case "sm":
       sizeValue = {
         width: pxToRem(36),
         height: pxToRem(36),
         fontSize: fontSize.sm,
-      };
-      break;
+      }
+      break
     case "lg":
       sizeValue = {
         width: pxToRem(58),
         height: pxToRem(58),
         fontSize: fontSize.sm,
-      };
-      break;
+      }
+      break
     case "xl":
       sizeValue = {
         width: pxToRem(74),
         height: pxToRem(74),
         fontSize: fontSize.md,
-      };
-      break;
+      }
+      break
     case "xxl":
       sizeValue = {
         width: pxToRem(110),
         height: pxToRem(110),
         fontSize: fontSize.md,
-      };
-      break;
+      }
+      break
+    case "myCustom":
+      sizeValue = {
+        width: pxToRem(150),
+        height: pxToRem(150),
+        fontSize: fontSize.md,
+      }
+      break
     default: {
       sizeValue = {
         width: pxToRem(48),
         height: pxToRem(48),
         fontSize: fontSize.md,
-      };
+      }
     }
   }
 
@@ -85,5 +92,5 @@ export default styled(Avatar)(({ theme, ownerState }) => {
     fontWeight: fontWeightRegular,
     boxShadow: boxShadows[shadow],
     ...sizeValue,
-  };
-});
+  }
+})
--- frontend/components/Molecules/layouts/Form/CreateSeller/index.jsx
+++ frontend/components/Molecules/layouts/Form/CreateSeller/index.jsx
@@ -1,27 +1,63 @@
-import React, { useState } from "react"
+import React, { useState, useRef } from "react"
+import { useFormik } from "formik"
+import * as yup from "yup"
 // @mui material components
 import Container from "@mui/material/Container"
 import Grid from "@mui/material/Grid"
-import Card from "@mui/material/Card"
-import Switch from "@mui/material/Switch"
 
 // Material Kit 2 React components
 import MKBox from "components/MKBox"
 import MKTypography from "components/MKTypography"
 import MKInput from "components/MKInput"
 import MKButton from "components/MKButton"
+import MKAvatar from "components/MKAvatar"
+
+// Import Icons
+import TwitterIcon from "@mui/icons-material/Twitter"
+import FacebookIcon from "@mui/icons-material/Facebook"
+import InstagramIcon from "@mui/icons-material/Instagram"
+
+// Import Image
+import defaultAvatar from "assets/images/default-avatar.png"
 
 function Seller() {
-  const [checked, setChecked] = useState(true)
+  const hiddenAvatarInput = useRef(null)
+  const handleClick = () => {
+    hiddenAvatarInput.current.click()
+  }
 
-  const handleChecked = () => setChecked(!checked)
-
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      email: "",
+      photo: "",
+      description: "",
+      twitter: "",
+      facebook: "",
+      instagram: "",
+    },
+    validationSchema: yup.object({
+      username: yup.string().required(""),
+      email: yup.string().email().required(""),
+      photo: yup.string(),
+      description: yup.string(),
+      twitter: yup.string(),
+      facebook: yup.string(),
+      instagram: yup.string(),
+    }),
+    enableReinitialize: true,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2))
+    },
+  })
+  const handleReset = () => {
+    formik.handleReset()
+  }
   return (
     <MKBox
       component="section"
       bgColor="white"
-      py={4}
-      px={10}
+      p={4}
       shadow="md"
       borderRadius="xl"
     >
@@ -40,46 +76,165 @@ function Seller() {
           </MKTypography>
         </Grid>
         <Grid container item xs={12} lg={10} sx={{ mx: "auto" }}>
-          <MKBox width="100%" component="form" method="post" autocomplete="off">
+          <MKBox
+            width="100%"
+            component="form"
+            method="post"
+            autoComplete="off"
+            onSubmit={formik.handleSubmit}
+            onReset={handleReset}
+          >
             <MKBox p={3}>
-              <Grid container spacing={3}>
-                <Grid item xs={12} md={6}>
-                  <Grid>
+              <Grid container>
+                <Grid item xs={12} md={6} pr={2}>
+                  <Grid pb={2}>
                     <MKTypography variant="h6" mb={1}>
                       Username
                     </MKTypography>
-                    <MKInput variant="outlined" fullWidth />
+                    <MKInput
+                      id="username"
+                      name="username"
+                      error={
+                        formik.touched.username &&
+                        Boolean(formik.errors.username)
+                      }
+                      onChange={formik.handleChange}
+                      value={formik.values.username}
+                      helperText={
+                        formik.touched.username && formik.errors.username
+                      }
+                      variant="outlined"
+                      fullWidth
+                      placeholder="Type something"
+                    />
                   </Grid>
-                  <Grid pt={3}>
+                  <Grid pb={2}>
                     <MKTypography variant="h6" mb={1}>
                       Email
                     </MKTypography>
-                    <MKInput variant="outlined" fullWidth />
+                    <MKInput
+                      id="email"
+                      name="email"
+                      error={
+                        formik.touched.email && Boolean(formik.errors.email)
+                      }
+                      onChange={formik.handleChange}
+                      value={formik.values.email}
+                      helperText={formik.touched.email && formik.errors.email}
+                      variant="outlined"
+                      fullWidth
+                      placeholder="Type something"
+                    />
                   </Grid>
                 </Grid>
-                <Grid item xs={12} md={6}></Grid>
-                <Grid item xs={12}>
+                <Grid item xs={12} md={6} pl={2}>
+                  <MKTypography variant="h6" mb={1}>
+                    Photo
+                  </MKTypography>
+                  <MKInput
+                    id="photo"
+                    name="photo"
+                    onChange={formik.handleChange}
+                    value={formik.values.photo}
+                    inputRef={hiddenAvatarInput}
+                    type="file"
+                    inputProps={{ accept: "image/png, image/gif, image/jpeg" }}
+                    variant="outlined"
+                    sx={{ position: "absolute", display: "none" }}
+                  />
+                  <MKAvatar
+                    src={defaultAvatar}
+                    alt="default-avatar"
+                    size="myCustom"
+                    onClick={handleClick}
+                    sx={{ margin: "auto" }}
+                  />
+                </Grid>
+                <Grid item xs={12} pb={2}>
                   <MKTypography variant="h6" mb={1}>
                     Description
                   </MKTypography>
                   <MKInput
+                    id="description"
+                    name="description"
+                    onChange={formik.handleChange}
+                    value={formik.values.description}
                     variant="outlined"
-                    type=""
                     multiline
                     fullWidth
                     rows={6}
+                    placeholder="Type something"
                   />
                 </Grid>
+                <Grid item xs={12} pb={2}>
+                  <MKTypography variant="h6" mb={1}>
+                    Social Media Link
+                  </MKTypography>
+                  <Grid container alignItems="center" pb={2}>
+                    <TwitterIcon fontSize="medium" sx={{ margin: "8px" }} />
+                    <Grid item flexGrow={1}>
+                      <MKInput
+                        id="twitter"
+                        name="twitter"
+                        onChange={formik.handleChange}
+                        value={formik.values.twitter}
+                        variant="outlined"
+                        fullWidth
+                        placeholder="Type something"
+                      />
+                    </Grid>
+                  </Grid>
+                  <Grid container alignItems="center" pb={2}>
+                    <FacebookIcon fontSize="medium" sx={{ margin: "8px" }} />
+                    <Grid item flexGrow={1}>
+                      <MKInput
+                        id="facebook"
+                        name="facebook"
+                        onChange={formik.handleChange}
+                        value={formik.values.facebook}
+                        variant="outlined"
+                        fullWidth
+                        placeholder="Type something"
+                      />
+                    </Grid>
+                  </Grid>
+                  <Grid container alignItems="center" pb={2}>
+                    <InstagramIcon fontSize="medium" sx={{ margin: "8px" }} />
+                    <Grid item flexGrow={1}>
+                      <MKInput
+                        id="instagram"
+                        name="instagram"
+                        onChange={formik.handleChange}
+                        value={formik.values.instagram}
+                        variant="outlined"
+                        fullWidth
+                        placeholder="Type something"
+                      />
+                    </Grid>
+                  </Grid>
+                </Grid>
               </Grid>
-              <Grid container item justifyContent="center" xs={12} my={2}>
-                <MKButton
-                  type="submit"
-                  variant="gradient"
-                  color="dark"
-                  fullWidth
-                >
-                  Send Message
-                </MKButton>
+              <Grid container justifyContent="end" my={2}>
+                <Grid item xs={3} mr={2}>
+                  <MKButton
+                    type="reset"
+                    variant="outlined"
+                    color="dark"
+                    fullWidth
+                  >
+                    Reset
+                  </MKButton>
+                </Grid>
+                <Grid item xs={3} ml={2}>
+                  <MKButton
+                    type="submit"
+                    variant="gradient"
+                    color="dark"
+                    fullWidth
+                  >
+                    Send
+                  </MKButton>
+                </Grid>
               </Grid>
             </MKBox>
           </MKBox>
